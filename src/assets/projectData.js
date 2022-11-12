408f169bf24a5c156e9d214a8eaa6a6a2b2ae0a4
chiragj07 <joshi.chirag54@gmail.com>
2022-11-12 15:57:05 +0530

    project-links-added

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -7,7 +7,7 @@ import ebooks from './ebooks.gif'
         id:"1",
         image:voting,
         description:"This is fullstack web platform for voting. It is built using ethereum blockchain and MERN Stack. Users can vote to their favourite candidate after loging in using their credntials.",
-        link:"https://github.com/chiragj07/election-dapp"
+        gitHubLink:"https://github.com/chiragj07/election-dapp"
     },
 
     {
@@ -15,7 +15,8 @@ import ebooks from './ebooks.gif'
         id:"2",
         image:ebooks,
         description:"A MERN Stack based e-commerce site where a shopkeeper can sell books and customer can buy it. Customer will have option to give ratings and review to the books and will have multiple payment options.",
-        link:"https://github.com/chiragj07/book-shop"
+        link:"https://book-e-shop.netlify.app/",
+        gitHubLink:"https://github.com/chiragj07/book-shop"
     },
     
         {
@@ -23,7 +24,9 @@ import ebooks from './ebooks.gif'
             id:"3",
             image:crypto,
             description:"It is a website for checking price of crypto currencies. User will be able to see the prices in different currencies, and will also have option to sort the coins based on price and price change percentage.",
-            link:"https://github.com/chiragj07/crypto-traker"
+            link:"https://crypto-chart-tracker.herokuapp.com/",
+            gitHubLink:"https://github.com/chiragj07/crypto-traker"
+
         }
     
 
